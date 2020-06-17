d92ee98514905168b7598fac39ecd39b7f8fb7cb
qidiaogufeng <1246029146@qq.com>
2020-06-17 08:47:29 +0800

    任晓刚git上传测试

---
+++ src/day03/HomeWork.java
@@ -0,0 +1,12 @@
+package day03;
+
+public class HomeWork {
+
+    public static void main(String[] args) {
+        //打印99乘法表。
+
+
+        System.out.println("打印成功！");
+        //打印等腰三角形。
+    }
+}
