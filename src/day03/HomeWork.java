f26e26a4ba87618c85c4de1dcf3d884cb7d8a7b7
qidiaogufeng <1246029146@qq.com>
2020-06-17 08:51:54 +0800

    任晓刚git上传测试第二次

--- src/day03/HomeWork.java
+++ src/day03/HomeWork.java
@@ -5,8 +5,9 @@ public class HomeWork {
     public static void main(String[] args) {
         //打印99乘法表。
 
-        System.out.println("打印成功2！")
+        System.out.println("打印成功2！");
         System.out.println("打印成功！");
+        System.out.println("第三次上传!");
         //打印等腰三角形。
     }
 }
